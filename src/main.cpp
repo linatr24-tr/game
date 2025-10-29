171e09e4499a34cc3b346056b4747558d1d4a35f
linatr24-tr <linatr24@gmail.com>
2025-10-29 00:31:30 +0000

    main.cpp ve kare.png güncellendi

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,13 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML 3.0.2 Test");
     window.setFramerateLimit(60);
-            
-std::cout<<"a";
+            sf::Texture texture("kare.png");
+            if (!texture.loadFromFile("kare.png")) {
+        std::cerr << "Hata: kare.png yüklenemedi!" << std::endl;
+        return -1; // Programı sonlandır
+    }
+            sf::Sprite sprite(texture);
+
    
 
     while (window.isOpen())
@@ -20,7 +25,7 @@ std::cout<<"a";
         }
 
         window.clear(sf::Color::Black);
-        
+        window.draw(sprite);
         window.display();
     }
 
